1f8aa9b561deff0ffcc4a974abfd2a9892e83d23
Andrei Vasilev <andrei.vasilev@nucleusdata.com>
2021-01-23 17:36:32 -0800

    Added production mode requirements to index.js

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ app.use(bodyParser.json());
 //IMPORT ROUTES
 // require('./routes/userRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+  app.use(express.static('client/build'));
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`app running on port ${PORT}`)
