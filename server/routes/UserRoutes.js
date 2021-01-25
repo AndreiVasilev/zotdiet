b9fd5cc29580d1569fd00ad0961c90d2a4ac7610
Andrei Vasilev <andrei.vasilev@nucleusdata.com>
2021-01-24 23:08:42 -0800

    Added login route to UserRoutes.js which passes authorization code to server side UserService

--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -1,7 +1,16 @@
 module.exports = (app) => {
 
-    app.post('/api/user/auth-code', async (req, res) => {
+    const userService = require('../services/UserService');
 
+    app.post('/api/user/login', async (req, res) => {
+        const result = await userService.login(req.body.code);
+        if (result) {
+            res.status(200);
+            res.send('login success');
+        } else {
+            res.status(500);
+            res.send('login failure');
+        }
     });
 
 }
