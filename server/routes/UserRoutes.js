42cd0aa99cf81e0fc1cf215b041abb9f4a541d8a
Andrei Vasilev <andrei.vasilev@nucleusdata.com>
2021-01-28 15:49:41 -0800

    Updated initial login logic between frontend and backend user services

--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -1,16 +1,21 @@
+const {userService} = require('../services/UserService');
+
 module.exports = (app) => {
 
-    const userService = require('../services/UserService');
-
     app.post('/api/user/login', async (req, res) => {
-        const result = await userService.login(req.body.code);
-        if (result) {
-            res.status(200);
-            res.send('login success');
-        } else {
-            res.status(500);
-            res.send('login failure');
-        }
+        // const result = await userService.login(req.body.code);
+        // if (result) {
+        //     res.status(200);
+        //     res.send('login success');
+        // } else {
+        //     res.status(500);
+        //     res.send('login failure');
+        // }
+    });
+
+    app.get('/api/user/loggedIn', async (req, res) => {
+        res.type('application/json')
+        res.json({loggedIn: req.session.loggedIn ? req.session.loggedIn : false});
     });
 
 }
