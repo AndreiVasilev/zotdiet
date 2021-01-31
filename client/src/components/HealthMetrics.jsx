52c47e8cd57b9ca224d6968e430a3165025fab51
Anika Vichare <anikavichare@gmail.com>
2021-01-30 18:57:49 -0800

    add health metrics page skeleton

---
+++ client/src/components/HealthMetrics.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HealthMetrics = () => {
+    return (
+        <div>
+            <h1>Health Metrics</h1>
+        </div>
+    )
+}
+
+export default HealthMetrics
