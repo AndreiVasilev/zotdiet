d7648ba670df9662cf967b916a7cd2f5cdfeaa8f
Andrei Vasilev <andrei.vasilev@nucleusdata.com>
2021-02-03 18:20:50 -0800

    Removed unused new user route.

--- client/src/routes.js
+++ client/src/routes.js
@@ -3,4 +3,3 @@ export const HOME = "/home";
 export const MEAL_PLAN = "/meal-plan";
 export const HEALTH_METRICS = "/health-metrics";
 export const PROFILE = "/profile";
-export const NEW_USER = "/new-user";
