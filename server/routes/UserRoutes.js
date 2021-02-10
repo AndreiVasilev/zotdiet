43b042e065be34ce33475e4af10d86dbefdb1c56
Andrei Vasilev <andrei.vasilev@nucleusdata.com>
2021-02-10 15:41:14 -0800

    Added not authorized method to check for session and user id

--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -3,60 +3,54 @@ const {userService} = require('../services/UserService');
 module.exports = (app) => {
 
     app.get('/api/user', async (req, res) => {
-        if (req.session && req.session.userId) {
-            const user = await userService.getUser(req.session.userId)
-                .catch(err => {
-                    console.err(`Unable to get user ${req.session.userId}`, err);
-                    res.status(404);
-                    res.send('User not found.');
-                });
-
-            if (user) {
-                res.json(user);
-            }
+        if (notAuthorized(req, res)) {
             return;
         }
 
-        res.status(401);
-        res.send('Session not found. Please login and try again');
+        const user = await userService.getUser(req.session.userId)
+            .catch(err => {
+                console.err(`Unable to get user ${req.session.userId}`, err);
+                res.status(404);
+                res.send('User not found.');
+            });
+
+        if (user) {
+            res.json(user);
+        }
     });
 
     app.post('/api/user', async (req, res) => {
-        if (req.session && req.session.userId) {
-            const user = await userService.createUser(req.body)
-                .catch(err => {
-                    console.err(`Unable to create user ${req.body.id}`, err);
-                    res.status(500);
-                    res.send('Unable to create user.');
-                });
-
-            if (user) {
-                res.json(user);
-            }
+        if (notAuthorized(req, res)) {
             return;
         }
 
-        res.status(401);
-        res.send('Session not found. Please login and try again');
+        const user = await userService.createUser(req.body)
+            .catch(err => {
+                console.err(`Unable to create user ${req.body.id}`, err);
+                res.status(500);
+                res.send('Unable to create user.');
+            });
+
+        if (user) {
+            res.json(user);
+        }
     });
 
     app.patch('/api/user', async (req, res) => {
-        if (req.session && req.session.userId) {
-            const user = await userService.updateUser(req.body)
-                .catch(err => {
-                    console.err(`Unable to update user ${req.body.id}`, err);
-                    res.status(500);
-                    res.send('Unable to update user.');
-                });
-
-            if (user) {
-                res.json(user);
-            }
+        if (notAuthorized(req, res)) {
             return;
         }
 
-        res.status(401);
-        res.send('Session not found. Please login and try again');
+        const user = await userService.updateUser(req.body)
+            .catch(err => {
+                console.err(`Unable to update user ${req.body.id}`, err);
+                res.status(500);
+                res.send('Unable to update user.');
+            });
+
+        if (user) {
+            res.json(user);
+        }
     });
 
     app.post('/api/user/login', async (req, res) => {
@@ -99,4 +93,13 @@ module.exports = (app) => {
         res.type('application/json')
         res.json({loggedIn: req.session.loggedIn ? req.session.loggedIn : false});
     });
+
+    function notAuthorized(req, res) {
+        if (!req.session || !req.session.userId) {
+            res.status(401);
+            res.send('Session not found. Please login and try again');
+            return true;
+        }
+        return false;
+    }
 }
