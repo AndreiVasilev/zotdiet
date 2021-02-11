7aa852aff93f498e3c5c37dd1ed4f6b2f608e2d7
Andrei Vasilev <andrei.vasilev@nucleusdata.com>
2021-02-10 17:56:17 -0800

    Created meal service

---
+++ server/services/MealService.js
@@ -0,0 +1,13 @@
+const {userService} = require('./UserService');
+const {spoonService} = require('./SpoonService');
+
+class MealService {
+
+
+
+
+}
+
+module.exports = {
+    mealService: new MealService()
+}
