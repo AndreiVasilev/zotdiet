8214d6bd29f390d7c1d1593de60f00f32ff17daa
Anika Vichare <anikavichare@gmail.com>
2021-02-12 22:49:25 -0800

    create dynamic graph functionality

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const spoonRouter = require('./server/routes/SpoonRoutes');
+const express = require("express");
+const bodyParser = require("body-parser");
+const spoonRouter = require("./server/routes/SpoonRoutes");
 const userRouter = require("./server/routes/UserRoutes");
-const session = require('express-session');
-const dotenv = require('dotenv');
+const session = require("express-session");
+const dotenv = require("dotenv");
 dotenv.config();
 
-const prodMode = process.env.NODE_ENV === 'production';
+const prodMode = process.env.NODE_ENV === "production";
 const app = express();
 app.use(bodyParser.json());
 
 // Initialize server side user session parameters
-app.use(session({
-  secret: process.env.EXPRESS_SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    httpOnly: true,
-    secure: prodMode,
-  }
-}));
+app.use(
+  session({
+    secret: process.env.EXPRESS_SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      httpOnly: true,
+      secure: prodMode,
+    },
+  })
+);
 
 // Initialize routes
-app.use('/api/user', userRouter);
-app.use('/spoon', spoonRouter);
+app.use("/api/user", userRouter);
+app.use("/spoon", spoonRouter);
 
 // Set application port
-app.set('port', process.env.PORT || 5000);
-app.listen(app.get('port'), () => {
-  console.log(`app running on port ${app.get('port')}`)
+app.set("port", process.env.PORT || 5000);
+app.listen(app.get("port"), () => {
+  console.log(`app running on port ${app.get("port")}`);
 });
-
-
 
 // Configure client to use correct build directory
 // if app is running in production mode
 if (prodMode) {
-  const path = require('path');
-  app.use(express.static('client/build'));
-  app.get('*', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
+  const path = require("path");
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -1,108 +1,146 @@
 const authHandler = require("./AuthHandler");
-const {userService} = require('../services/UserService');
-const userRouter = require('express').Router();
+const { userService } = require("../services/UserService");
+const userRouter = require("express").Router();
 
-userRouter.get('/', [authHandler, async (req, res) => {
-    const user = await userService.getUser(req.session.userId)
-        .catch(err => {
-            console.error(`Unable to get user ${req.session.userId}`, err);
-            res.status(404);
-            res.send('User not found.');
-        });
+userRouter.get("/", [
+  authHandler,
+  async (req, res) => {
+    const user = await userService.getUser(req.session.userId).catch((err) => {
+      console.error(`Unable to get user ${req.session.userId}`, err);
+      res.status(404);
+      res.send("User not found.");
+    });
 
     if (user) {
-        res.json(user);
+      res.json(user);
     }
-}]);
+  },
+]);
 
-userRouter.get('/steps', [authHandler, async (req, res) => {
-    const steps = await userService.getUserSteps(req.session.accessToken, req.query.lastNumDays)
-        .catch(err => {
-            console.error(`Unable to get steps of user ${req.session.userId}`, err);
-            res.status(500);
-        });
+userRouter.get("/steps", [
+  authHandler,
+  async (req, res) => {
+    const steps = await userService
+      .getUserSteps(req.session.accessToken, req.query.lastNumDays)
+      .catch((err) => {
+        console.error(`Unable to get steps of user ${req.session.userId}`, err);
+        res.status(500);
+      });
 
     if (steps) {
-        res.json(steps);
+      res.json([steps]);
     }
-}]);
+  },
+]);
 
-userRouter.get('/meal-plan', [authHandler, async (req, res) => {
-    const mealPlan = await userService.getMealPlan(req.session.userId, req.session.accessToken)
-        .catch(err => {
-            console.error(`Unable to get meal plan for user ${req.session.userId}`, err);
-            res.status(500);
-        });
+userRouter.get("/weight", [
+  authHandler,
+  async (req, res) => {
+    const weight = await userService
+      .getUserWeight(req.session.accessToken, req.query.lastNumDays)
+      .catch((err) => {
+        console.error(
+          `Unable to get weight of user ${req.session.userId}`,
+          err
+        );
+        res.status(500);
+      });
+
+    if (weight) {
+      res.json(weight);
+    }
+  },
+]);
+
+userRouter.get("/meal-plan", [
+  authHandler,
+  async (req, res) => {
+    const mealPlan = await userService
+      .getMealPlan(req.session.userId, req.session.accessToken)
+      .catch((err) => {
+        console.error(
+          `Unable to get meal plan for user ${req.session.userId}`,
+          err
+        );
+        res.status(500);
+      });
 
     if (mealPlan) {
-        res.json(mealPlan);
+      res.json(mealPlan);
     }
-}]);
+  },
+]);
 
-userRouter.post('/', [authHandler, async (req, res) => {
-    const user = await userService.createUser(req.body)
-        .catch(err => {
-            console.error(`Unable to create user ${req.body.id}`, err);
-            res.status(500);
-            res.send('Unable to create user.');
-        });
+userRouter.post("/", [
+  authHandler,
+  async (req, res) => {
+    const user = await userService.createUser(req.body).catch((err) => {
+      console.error(`Unable to create user ${req.body.id}`, err);
+      res.status(500);
+      res.send("Unable to create user.");
+    });
 
     if (user) {
-        res.json(user);
+      res.json(user);
     }
-}]);
+  },
+]);
 
-userRouter.patch('/', [authHandler, async (req, res) => {
-    const user = await userService.updateUser(req.body)
-        .catch(err => {
-            console.error(`Unable to update user ${req.body.id}`, err);
-            res.status(500);
-            res.send('Unable to update user.');
-        });
+userRouter.patch("/", [
+  authHandler,
+  async (req, res) => {
+    const user = await userService.updateUser(req.body).catch((err) => {
+      console.error(`Unable to update user ${req.body.id}`, err);
+      res.status(500);
+      res.send("Unable to update user.");
+    });
 
     if (user) {
-        res.json(user);
+      res.json(user);
     }
-}]);
+  },
+]);
 
-userRouter.post('/login', async (req, res) => {
+userRouter.post("/login", async (req, res) => {
+  // Attempt to login by creating Google access token
+  const loginResult = await userService.login(req.body.code);
+  if (!loginResult) {
+    res.status(500);
+    res.send("Login failed.");
+    return;
+  }
 
-    // Attempt to login by creating Google access token
-    const loginResult = await userService.login(req.body.code);
-    if (!loginResult) {
-        res.status(500);
-        res.send('Login failed.');
-        return;
-    }
+  // Get Google user info and determine if user already
+  // exists with that id in our database
+  const googleUser = loginResult.googleUser;
+  let user = await userService.getUser(googleUser.id);
+  let isNew = !user;
 
-    // Get Google user info and determine if user already
-    // exists with that id in our database
-    const googleUser = loginResult.googleUser;
-    let user = await userService.getUser(googleUser.id);
-    let isNew = !user;
-
-    // Store user related information in their session
-    req.session.accessToken = loginResult.token;
-    req.session.userId = googleUser.id;
-    req.session.loggedIn = true;
-    res.json({isNew: isNew, initUser: googleUser});
+  // Store user related information in their session
+  req.session.accessToken = loginResult.token;
+  req.session.userId = googleUser.id;
+  req.session.loggedIn = true;
+  res.json({ isNew: isNew, initUser: googleUser });
 });
 
-userRouter.post('/logout', [authHandler, async (req, res) => {
-    req.session.destroy(function(err) {
-        if (err) {
-            console.error(err);
-            res.status(500);
-            res.send('Logout failed. Unable to destroy server session');
-        } else {
-            res.send('Logout success');
-        }
+userRouter.post("/logout", [
+  authHandler,
+  async (req, res) => {
+    req.session.destroy(function (err) {
+      if (err) {
+        console.error(err);
+        res.status(500);
+        res.send("Logout failed. Unable to destroy server session");
+      } else {
+        res.send("Logout success");
+      }
     });
-}]);
+  },
+]);
 
-userRouter.get('/loggedIn', async (req, res) => {
-    res.type('application/json')
-    res.json({loggedIn: req.session.loggedIn ? req.session.loggedIn : false});
+userRouter.get("/loggedIn", async (req, res) => {
+  res.type("application/json");
+  res.json({ loggedIn: req.session.loggedIn ? req.session.loggedIn : false });
 });
 
 module.exports = userRouter;
