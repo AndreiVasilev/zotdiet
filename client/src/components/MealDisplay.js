5f44397ddfb54bde160d6381bd0fe52cf5a5646a
Amy Weitzman <amyweitzman@yahoo.com>
2021-02-12 21:09:33 -0800

    add thumbs up/down icons to meal plan

--- client/src/components/MealDisplay.js
+++ client/src/components/MealDisplay.js
@@ -1,13 +1,37 @@
-import React  from "react";
+import React, { useState }  from "react";
 import "./MealDisplay.css";
 import "../App.css";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faClock} from "@fortawesome/free-solid-svg-icons/faClock";
+import {faThumbsUp} from "@fortawesome/free-solid-svg-icons/faThumbsUp";
+import {faThumbsDown} from "@fortawesome/free-solid-svg-icons/faThumbsDown";
 
 import MealDisplayImg from "./MealDisplayImg";
 
 function MealDisplay(props) {
     const { mealType, mealName, url, img, cookTime, openModal } = props;
+    const [thumbsUp, setThumbsUp] = useState(false); //TODO set to current status
+    const [thumbsDown, setThumbsDown] = useState(false); //TODO set to current status
+
+    const toggleThumbs = (dir) => {
+        // cannot have both thumbs up and thumbs down selected
+        switch(dir) {
+            case "up":
+                setThumbsUp(!thumbsUp);
+                setThumbsDown(false);
+                // TODO set in DB
+                break;
+            case "down":
+                setThumbsDown(!thumbsDown);
+                setThumbsUp(false);
+                // TODO set in DB
+                break;
+            default:
+                setThumbsDown(false);
+                setThumbsDown(false);
+        }
+    };
+
     return (
         <div className="meal-display-container" onClick={openModal}>
             <p className="bold-text meal-display-text">{mealType}</p>
@@ -16,9 +40,17 @@ function MealDisplay(props) {
                 <p className="bold-text meal-display-text">{mealName}</p>
             </a>
             <p className="meal-display-text meal-display-text-sm">
-                <FontAwesomeIcon icon={faClock} size="3x" className="meal-time-icon" />
+                <FontAwesomeIcon icon={faClock} size="3x" className="meal-display-icon meal-time-icon" />
                 Ready in {cookTime} minutes
             </p>
+            <div className="thumbs-container">
+                <FontAwesomeIcon icon={faThumbsUp} size="3x" onClick={() => toggleThumbs("up")}
+                     className={`meal-display-icon meal-thumbs-icon ${thumbsUp ? "thumbs-selected" : "thumbs-unselected"}`}
+                />
+                <FontAwesomeIcon icon={faThumbsDown} size="3x" onClick={() => toggleThumbs("down")}
+                     className={`meal-display-icon meal-thumbs-icon ${thumbsDown ? "thumbs-selected" : "thumbs-unselected"}`}
+                />
+            </div>
         </div>
     );
 }
